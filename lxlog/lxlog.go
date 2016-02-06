1b54e5d7362067ed0827030465314073dc16fbf3
Scott Weiss <scott.weiss@emc.com>
2016-02-06 12:39:06 -0500

    allow adding extra loggers to logging
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- lxlog/lxlog.go
+++ lxlog/lxlog.go
@@ -5,20 +5,79 @@ import (
 	"github.com/Sirupsen/logrus"
 	"runtime"
 	"strings"
+	"io"
+	"net/http"
+	"os/exec"
+	"bufio"
 )
 
 var log = logrus.New()
+var optionalLogs = make(map[string]*logrus.Logger)
 
 func ActiveDebugMode() {
 	log.Level = logrus.DebugLevel
+}
+
+func AddLogger(name string, logLevel logrus.Level, w io.Writer) {
+	newLogger := logrus.New()
+	newLogger.Out = w
+	newLogger.Level = logLevel
+	optionalLogs[name] = newLogger
+}
+
+func LogCommand(cmd *exec.Cmd, debug bool) {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return
+	}
+	go func() {
+		// read command's stdout line by line
+		in := bufio.NewScanner(stdout)
+
+		for in.Scan() {
+			if debug {
+				Debugf(logrus.Fields{}, in.Text())
+			} else {
+				Infof(logrus.Fields{}, in.Text())
+			}
+		}
+	}()
+	go func() {
+		// read command's stdout line by line
+		in := bufio.NewScanner(stderr)
+
+		for in.Scan() {
+			Errorf(logrus.Fields{}, in.Text())
+		}
+	}()
+}
+
+func DeleteLogger(name string) {
+	delete(optionalLogs, name)
 }
 
 func Debugf(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Debugf(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Debugf(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Debug(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Debug(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
@@ -26,8 +85,20 @@ func Infof(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Infof(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Infof(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Info(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Info(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
@@ -35,8 +106,20 @@ func Warnf(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Warnf(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Warnf(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Warn(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Warn(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
@@ -44,8 +127,20 @@ func Errorf(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Errorf(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Errorf(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Error(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Error(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
@@ -53,8 +148,20 @@ func Fatalf(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Fatalf(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Fatalf(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Fatal(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Fatal(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
@@ -62,8 +169,20 @@ func Panicf(fields logrus.Fields, format string, a ...interface{}) {
 	format = addTrace(format)
 	if len(a) > 0 {
 		log.WithFields(fields).Panicf(format, a)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Panicf(format, a)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	} else {
 		log.WithFields(fields).Panic(format)
+		for _, optionalLog := range optionalLogs {
+			optionalLog.WithFields(fields).Panic(format)
+			if flusher, ok := optionalLog.Out.(http.Flusher); ok {
+				flusher.Flush()
+			}
+		}
 	}
 }
 
