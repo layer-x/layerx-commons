2620303eb574313f8f6bb68e33c0fd26a50d4e6f
Scott Weiss <scott.weiss@emc.com>
2016-03-13 20:21:27 -0400

    add tee
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ lxstream/tee.go
@@ -0,0 +1,31 @@
+package lxstream
+import (
+	"os"
+	"io"
+"bytes"
+	"bufio"
+"github.com/layer-x/layerx-commons/lxerrors"
+)
+
+func Tee(file *os.File, buf *bytes.Buffer) error {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return lxerrors.New("creating pipe", err)
+	}
+	stdout := file
+	file = w
+	multi := io.MultiWriter(stdout, bufio.NewWriter(buf))
+	reader := bufio.NewReader(r)
+	go func() {
+		for {
+			line, err := reader.ReadBytes('\n')
+			if err != nil {
+				return
+			}
+			_, err = multi.Write(append(line, byte('\n')))
+			if err != nil {
+				return
+			}
+		}
+	}()
+}
