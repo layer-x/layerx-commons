e1a4a9fdc5a924541dbbd1317c56369e49e56688
Scott Weiss <scott.weiss@emc.com>
2016-04-02 18:27:16 -0400

    add loglines
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- lxerrors/errors.go
+++ lxerrors/errors.go
@@ -1,23 +1,42 @@
 package lxerrors
 
-import "fmt"
+import (
+"fmt"
+"runtime"
+"strings"
+)
 
-type tpiError struct {
+type lxerror struct {
 	error
 	message string
 	err     error
+	file    string
 }
 
-func New(message string, err error) *tpiError {
-	return &tpiError{
+func New(message string, err error) *lxerror {
+	return &lxerror{
 		message: message,
 		err:     err,
+		file: getTrace(),
 	}
 }
 
-func (e *tpiError) Error() string {
+func (e *lxerror) Error() string {
 	if e.err != nil {
-		return fmt.Sprintf("%s: {%s}", e.message, e.err.Error())
+		return fmt.Sprintf("%s %s: {%s}", e.file, e.message, e.err.Error())
 	}
-	return fmt.Sprintf("%s", e.message)
+	return fmt.Sprintf("%s %s", e.file, e.message)
+}
+
+func getTrace() string {
+	_, fn, line, _ := runtime.Caller(2)
+	pathComponents := strings.Split(fn, "/")
+	var truncatedPath string
+	if len(pathComponents) > 3 {
+		truncatedPath = strings.Join(pathComponents[len(pathComponents) - 2:], "/")
+	} else {
+		truncatedPath = strings.Join(pathComponents, "/")
+	}
+	file := fmt.Sprintf("[%s:%d]", truncatedPath, line)
+	return file
 }
