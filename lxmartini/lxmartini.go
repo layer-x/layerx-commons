74e4bd267348b1ebac669568d0e324672a73cea2
Scott Weiss <scott.weiss@emc.com>
2016-04-03 15:50:41 -0400

    fix martini wrapper
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- lxmartini/lxmartini.go
+++ lxmartini/lxmartini.go
@@ -2,7 +2,6 @@ package lxmartini
 
 import (
 	"fmt"
-	"github.com/Sirupsen/logrus"
 	"github.com/go-martini/martini"
 	"github.com/layer-x/layerx-commons/lxlog"
 	"net/http"
@@ -25,7 +24,7 @@ func QuietMartini() *martini.ClassicMartini {
 func customLogger() martini.Handler {
 	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
 		start := time.Now()
-
+		logger := lxlog.New()
 		addr := req.Header.Get("X-Real-IP")
 		if addr == "" {
 			addr = req.Header.Get("X-Forwarded-For")
@@ -34,12 +33,12 @@ func customLogger() martini.Handler {
 			}
 		}
 
-		lxlog.Debugf(logrus.Fields{}, fmt.Sprintf("Started %s %s for %s", req.Method, req.URL.Path, addr))
+		logger.Debugf(fmt.Sprintf("Started %s %s for %s", req.Method, req.URL.Path, addr))
 
 		rw := res.(martini.ResponseWriter)
 		c.Next()
 
-		lxlog.Debugf(logrus.Fields{}, fmt.Sprintf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start)))
+		logger.Debugf(fmt.Sprintf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start)))
 	}
 }
 
